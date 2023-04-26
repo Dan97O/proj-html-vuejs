3596de7688cf56b35dce855e1e60390e3ca404e8
Dan97O <dan97-oddo@live.it>
2023-04-26 15:25:20 +0200

    add button prev and next header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,13 +10,14 @@ export default {
       <button class="btn rounded-0 me-5">ORDER ONLINE</button>
       <ul class="d-flex gap-3 m-0">
         <li>
-          <a class="d-flex me-5" href="#"><img class="img-fluid me-2" src="../assets/svg/svg-0.svg" alt=""> HOME</a>
+          <a class="d-flex align-items-center" href="#"><img style="height: 13px;" class="img-fluid me-2"
+              src="../assets/svg/svg-0.svg" alt=""> HOME</a>
         </li>
         <li><a href="#">PAGES</a></li>
         <li><a href="#">MENU</a></li>
       </ul>
       <img class="logo mx-3" src="../assets/img/h5-logo-divided-header.png" alt="">
-      <ul class="d-flex gap-3 m-0 me-5">
+      <ul class="d-flex gap-3 m-0 me-5 p-0">
         <li><a href="#">EVENT</a></li>
         <li><a href="#">BLOG</a></li>
         <li><a href="#">LANDING</a></li>
@@ -30,9 +31,20 @@ export default {
         </li>
       </ul>
     </nav>
-    <div>
+    <section class="d-flex justify-content-center">
+      <div class="advance d-flex justify-content-between">
+        <div class="prev d-flex align-items-center">
+          <button>PREV</button>
+        </div>
+        <div class="carosel">
+          <img height="400px" src="../assets/img/h3-rev-img-4.png" alt="">
+        </div>
+        <div class="next d-flex align-items-center">
+          <button>NEXT</button>
+        </div>
+      </div>
 
-    </div>
+    </section>
 
   </header>
 </template>
@@ -44,8 +56,9 @@ export default {
 
 header {
 
-  height: 500px;
+  height: 600px;
   background-color: black;
+
 
   nav {
     display: flex;
@@ -74,6 +87,41 @@ header {
 
     .logo {
       height: 130px;
+    }
+  }
+
+  .advance {
+    width: 100%;
+
+    .prev button {
+      padding: 10px;
+      background-color: $header_text_color;
+      color: $header_btn;
+      border: none;
+      border-radius: 50% 50% 0 0;
+      writing-mode: vertical-rl;
+      transform: rotate(90deg);
+      height: 40px;
+      margin-left: -8px;
+    }
+
+    .next button {
+      padding: 10px;
+      background-color: $header_text_color;
+      color: $header_btn;
+      border: none;
+      border-radius: 50% 50% 0 0;
+      writing-mode: vertical-rl;
+      transform: rotate(-90deg);
+      height: 40px;
+      margin-right: -8px;
+    }
+
+    .carosel {
+      text-align: center;
+      background-image: url(../assets/img/h3-rev-img-5.png);
+      background-repeat: no-repeat;
+      width: 53%;
     }
   }
 
