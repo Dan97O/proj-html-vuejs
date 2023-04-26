215480cfa696bb8fa497fff8eb06227bcfa403e2
Dan97O <dan97-oddo@live.it>
2023-04-26 12:35:28 +0200

    add navbar header

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import './assets/scss/app.scss'
 import App from './App.vue'
-import 'bootstrap/dist/js/bootstrap.min.js'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '../node_modules/bootstrap/dist/js/bootstrap'
 
 
 createApp(App).mount('#app')
