3ff607b951e087e8f860065171f4ecb3d827b170
Dan97O <dan97-oddo@live.it>
2023-04-26 20:21:28 +0200

    add app main section 'new place'

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,23 @@
 <script>
+import NewPlace from './NewPlace.vue';
+import SectionMagazine from './SectionMagazine.vue'
 export default {
+  name: 'AppMain',
+  components: {
+    NewPlace,
+    SectionMagazine
+  }
 
 }
 </script>
-<template></template>
+
+<template>
+  <main>
+    <NewPlace />
+    <SectionMagazine />
+  </main>
+</template>
+
 
 
 <style lang="scss" scoped></style>
---
+++ src/components/NewPlace.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+  <div class="container-fluid p-0">
+    <div class="row mt-2">
+      <div class="col-3 p-0 pe-2">
+        <a href="#"><img class="img-fluid" src="../assets/img/h3-img-1.jpg" alt=""></a>
+      </div>
+      <!-- /.col-3 -->
+      <div class="col-3 p-0 pe-2">
+        <a href="#"><img class="img-fluid" src="../assets/img/h3-img-2.jpg" alt=""></a>
+      </div>
+      <!-- /.col-3 -->
+      <div class="col-3 p-0 pe-2">
+        <a href="#"><img class="img-fluid" src="../assets/img/h3-img-3.jpg" alt=""></a>
+      </div>
+      <!-- /.col-3 -->
+      <div class="col-3 p-0 pe-2">
+        <a href="#"><img class="img-fluid" src="../assets/img/h3-img-4.jpg" alt=""></a>
+      </div>
+      <!-- /.col-3 -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+</template>
+
+
+<style lang="scss" scoped></style>
