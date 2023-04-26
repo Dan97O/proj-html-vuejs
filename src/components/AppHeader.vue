a61dbfd05f335e4bf13089c2b8f66624e5728f07
Dan97O <dan97-oddo@live.it>
2023-04-26 16:15:58 +0200

    add components NavbarHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,36 +1,17 @@
 <script>
+import NavbarHeader from './NavbarHeader.vue';
 export default {
+  name: 'AppHeader',
+  components: {
+    NavbarHeader
+  },
 
 }
 </script>
 <template>
   <header>
 
-    <nav class="d-flex justify-content-center align-items-center py-4">
-      <button class="btn rounded-0 me-5">ORDER ONLINE</button>
-      <ul class="d-flex gap-3 m-0">
-        <li>
-          <a class="d-flex align-items-center" href="#"><img style="height: 13px;" class="img-fluid me-2"
-              src="../assets/svg/svg-0.svg" alt=""> HOME</a>
-        </li>
-        <li><a href="#">PAGES</a></li>
-        <li><a href="#">MENU</a></li>
-      </ul>
-      <img class="logo mx-3" src="../assets/img/h5-logo-divided-header.png" alt="">
-      <ul class="d-flex gap-3 m-0 me-5 p-0">
-        <li><a href="#">EVENT</a></li>
-        <li><a href="#">BLOG</a></li>
-        <li><a href="#">LANDING</a></li>
-      </ul>
-      <ul class="d-flex gap-3 m-0">
-        <li>
-          <a class="d-flex" href="#"><img class="img-fluid me-2" src="../assets/svg/svg-1.svg" alt=""> CART</a>
-        </li>
-        <li>
-          <a class="d-flex" href="#"><i class="fa-solid fa-magnifying-glass fa-rotate-90 me-2"></i> SEARCH</a>
-        </li>
-      </ul>
-    </nav>
+    <NavbarHeader />
     <section class="d-flex justify-content-center">
       <div class="advance d-flex justify-content-between">
         <div class="prev d-flex align-items-center">
@@ -43,7 +24,6 @@ export default {
           <button>NEXT</button>
         </div>
       </div>
-
     </section>
 
   </header>
@@ -58,37 +38,6 @@ header {
 
   height: 600px;
   background-color: black;
-
-
-  nav {
-    display: flex;
-    justify-content: center;
-
-    button {
-      background-color: $header_btn;
-      color: $header_text_color;
-    }
-
-    ul {
-      list-style: none;
-
-      a {
-        text-decoration: none;
-        color: $header_text_color;
-
-
-        img {
-          color: white;
-          background-color: white;
-          filter: invert(1);
-        }
-      }
-    }
-
-    .logo {
-      height: 130px;
-    }
-  }
 
   .advance {
     width: 100%;
---
+++ src/components/NavbarHeader.vue
@@ -0,0 +1,101 @@
+<script>
+export default {
+  name: 'NavbarHeader',
+
+  data() {
+    return {
+      pages: [
+        {
+          title: 'HOME',
+          url: '#'
+        },
+        {
+          title: 'PAGES',
+          url: '#'
+        },
+        {
+          title: 'MENU',
+          url: '#'
+        }
+      ],
+
+      links: [
+        {
+          title: 'EVENT',
+          url: '#'
+        },
+        {
+          title: 'BLOG',
+          url: '#'
+        },
+        {
+          title: 'LANDING',
+          url: '#'
+        }
+      ]
+    };
+  }
+}
+</script>
+<template>
+  <nav class="d-flex justify-content-center align-items-center py-4">
+    <button class="btn rounded-0 me-5">ORDER ONLINE</button>
+    <ul class="d-flex gap-3 m-0">
+      <li>
+        <a class="d-flex align-items-center" :href="pages[0].url"><img style="height: 13px;" class="img-fluid me-2"
+            src="../assets/svg/svg-0.svg" :alt="pages[0].title"> {{ pages[0].title }}</a>
+      </li>
+      <li><a :href="pages[1].url"> {{ pages[1].title }}</a></li>
+      <li><a :href="pages[2].url"> {{ pages[2].title }}</a></li>
+    </ul>
+    <img class="logo mx-3" src="../assets/img/h5-logo-divided-header.png" alt="">
+    <ul class="d-flex gap-3 m-0 me-5 p-0">
+      <li><a :href="links[0].url">{{ links[0].title }}</a></li>
+      <li><a :href="links[1].url">{{ links[1].title }}</a></li>
+      <li><a :href="links[2].url">{{ links[2].title }}</a></li>
+    </ul>
+    <ul class="d-flex gap-3 m-0">
+      <li>
+        <a class="d-flex" href="#"><img class="img-fluid me-2" src="../assets/svg/svg-1.svg" alt=""> CART</a>
+      </li>
+      <li>
+        <a class="d-flex" href="#"><i class="fa-solid fa-magnifying-glass fa-rotate-90 me-2"></i> SEARCH</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+
+<style lang="scss" scoped>
+@import "../assets/scss/partials/variables";
+
+nav {
+  display: flex;
+  justify-content: center;
+
+  button {
+    background-color: $header_btn;
+    color: $header_text_color;
+  }
+
+  ul {
+    list-style: none;
+
+    a {
+      text-decoration: none;
+      color: $header_text_color;
+
+
+      img {
+        color: white;
+        background-color: white;
+        filter: invert(1);
+      }
+    }
+  }
+
+  .logo {
+    height: 130px;
+  }
+}
+</style>
